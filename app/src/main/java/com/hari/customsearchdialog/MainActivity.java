8f60ea2b1eb50587516e1a4620c59b67b547f4df
HARI <hk.mca08@gmail.com>
2020-04-29 11:57:13 +0530

    Camera-x Demo Added

--- app/src/main/java/com/hari/customsearchdialog/MainActivity.java
+++ app/src/main/java/com/hari/customsearchdialog/MainActivity.java
@@ -1,8 +1,10 @@
 package com.hari.customsearchdialog;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.hari.customsearchdialog.dialog.CitySearchDialog;
@@ -19,17 +21,20 @@ public class MainActivity extends AppCompatActivity {
     List<CityItem> cityList = new ArrayList<>();
     String selectedCityId;
     EditText cityET;
+    Button cameraxBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activity = this;
         setContentView(R.layout.activity_main);
+        cameraxBTN = findViewById(R.id.cameraxBTN);
         cityET = findViewById(R.id.cityET);
         cityET.setFocusable(false);
 
         addItemInList();
         openDialog(null);
+        cameraxBTN.setOnClickListener(v -> startActivity(new Intent(activity, CameraxActivity.class)));
         cityET.setOnClickListener(v -> openDialog(v));
     }
 
